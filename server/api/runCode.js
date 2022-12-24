f4e3ec4f72e6b862e342e968ab9bfbb1184f4c2f
rabi27 <rabisiddique27@gmail.com>
2022-12-24 14:41:45 +0500

    server side logic to create code files

--- server/api/runCode.js
+++ server/api/runCode.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+
+const languages = {
+  python: '.py',
+  javascript: '.js',
+};
+
 module.exports = (req, res) => {
-  const { code } = req.body;
-  console.log(code);
+  const { code, language } = req.body;
+  const filename = __dirname + `\\Files\\main${languages[language]}`;
+  fs.writeFile(filename, code, function (err) {
+    console.error(err);
+  });
   res.send('Hello World');
 };
