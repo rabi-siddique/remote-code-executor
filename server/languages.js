c1dc4004494e3f627c9665257d59683ee25c77dd
rabi27 <rabisiddique27@gmail.com>
2023-02-25 08:32:19 +0500

    update language.js

--- server/languages.js
+++ server/languages.js
@@ -1,16 +1,19 @@
 const extensions = {
   python: '.py',
   javascript: '.js',
+  go: '.go',
 };
 
 const commands = {
   python: 'python3',
   javascript: 'node',
+  go: 'go run',
 };
 
 const containerNames = {
   python: 'python:v1',
-  javascript: 'js:v1',
+  javascript: 'node:v1',
+  go: 'go:v1',
 };
 
 module.exports = { extensions, commands, containerNames };
